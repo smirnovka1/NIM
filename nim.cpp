ca5da4e6302b4a2209a265dc95b1ca9a9adaba0e
smirnovphystech <smirnov.phystech@gmail.com>
2023-09-30 09:28:53 +0300

    nim

--- nim.cpp
+++ nim.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
-#include <stdexcept>
 #include <vector>
-#include <string>
 #include <cstring>
 
 using namespace std;
 
-vector<int> rows_num = {3, 4, 5};
+vector<int> rocks_num = {3, 4, 5};
 int current_move;
 
 void start();
@@ -49,7 +47,7 @@ void start()
     cout << string(10, '#') << string(10, ' ') << "ИГРА НИМ" << string(10, ' ') <<
          string(10, '#') << endl << endl;
     cout << "ПРАВИЛА ИГРЫ:" << endl;
-    cout << "ДВА ИГРОКА ПО ОЧЕРЕДИ ЗАБИРАЮТ ОДНУ ИЛИ НЕСКОЛЬКО ФИШЕК" << endl
+    cout << "ДВА ИГРОКА ПО ОЧЕРЕДИ ЗАБИРАЮТ ОДИН ИЛИ НЕСКОЛЬКО КАМНЕЙ" << endl
          << "ИЗ ЛЮБОГО РЯДА. НЕ РАЗРЕШАЕТСЯ ЗА ОДИН ХОД БРАТЬ ФИШКИ ИЗ НЕСКОЛЬКИХ РЯДОВ." << endl
          << "ВЫИГРЫВАЕТ ТОТ, КТО ПОСЛЕДНИЙ ВОЗЬМЁТ ФИШКУ" << endl << endl;
 }
@@ -57,9 +55,9 @@ void start()
 int nim_sum()
 {
     int nim = 0;
-    for (int i = 0; i < rows_num.size(); i++)
+    for (int i = 0; i < rocks_num.size(); i++)
     {
-        nim = nim ^ rows_num[i];
+        nim = nim ^ rocks_num[i];
     }
     return nim;
 }
@@ -77,17 +75,17 @@ void choose_game()
 
 void show_pieces()
 {
-    cout << "ТЕКУЩЕЕ КОЛИЧЕСТВО КАМНЕЙ В КУЧАХ" << endl << "1.   " << string(rows_num[0], '#') <<
-         "   (" << rows_num[0] << ")" << endl << "2.   " << string(rows_num[1], '#') << "   (" <<
-         rows_num[1] << ")" << endl << "3.   " << string(rows_num[2], '#') << "   (" << rows_num[2] << ")"
+    cout << "ТЕКУЩЕЕ КОЛИЧЕСТВО КАМНЕЙ В КУЧАХ" << endl << "1.   " << string(rocks_num[0], '#') <<
+         "   (" << rocks_num[0] << ")" << endl << "2.   " << string(rocks_num[1], '#') << "   (" <<
+         rocks_num[1] << ")" << endl << "3.   " << string(rocks_num[2], '#') << "   (" << rocks_num[2] << ")"
          << endl << endl;
 }
 
 bool game_ended()
 {
-    for (int i = 0; i < rows_num.size(); i++)
+    for (int i = 0; i < rocks_num.size(); i++)
     {
-        if (rows_num[i] > 0)
+        if (rocks_num[i] > 0)
         {
             return false;
         }
@@ -98,72 +96,72 @@ bool game_ended()
 void player_move()
 {
     vector<int> good_rows;
-    for(int i = 0; i<rows_num.size(); i++){
-        if (rows_num[i] != 0)
+    for(int i = 0; i < rocks_num.size(); i++){
+        if (rocks_num[i] != 0)
             good_rows.push_back(i+1);
         else{
             good_rows.push_back(0);
         }
     }
 
-    string num;
-    char *char_num;
+    string row_number;
+    char *char_row_number;
     cout << "Введите номер кучи, из которой хотите взять камни.\n";
     while (cin) {
-        cin >> num;
-        if (num.size() == 1) {
-            char_num = new char[num.length() + 1];
-            strcpy(char_num, num.c_str());
-            if ((char_num[0] > '0') and (char_num[0] <= '9') and (count(good_rows.begin(),good_rows.end(),char_num[0]-'0'))) {
+        cin >> row_number;
+        if (row_number.size() == 1) {
+            char_row_number = new char[row_number.length() + 1];
+            strcpy(char_row_number, row_number.c_str());
+            if ((char_row_number[0] > '0') and (char_row_number[0] <= '9') and (count(good_rows.begin(), good_rows.end(), char_row_number[0] - '0'))) {
                 break;
             }
         }
         cout << "Неправильный ввод, попробуйте ещё раз" << endl;
     }
-    int resnum = char_num[0] - '0';
+    int res_row_number = char_row_number[0] - '0';
 
-    string count1;
-    char *char_count;
+    string rocks_number;
+    char *char_rocks_number;
     cout << "Введите количество камней, которое хотите взять.\n";
     while (cin) {
-        cin >> count1;
-        if (count1.size() == 1) {
-            char_count = new char[count1.length() + 1];
-            strcpy(char_count, count1.c_str());
-            int resc = char_count[0] - '0';
-            if ((char_count[0] > '0') and (char_count[0] <= '9') and (rows_num[resnum-1] >= resc)) {
+        cin >> rocks_number;
+        if (rocks_number.size() == 1) {
+            char_rocks_number = new char[rocks_number.length() + 1];
+            strcpy(char_rocks_number, rocks_number.c_str());
+            int resc = char_rocks_number[0] - '0';
+            if ((char_rocks_number[0] > '0') and (char_rocks_number[0] <= '9') and (rocks_num[res_row_number - 1] >= resc)) {
                 break;
             }
         }
         cout << "Неправильный ввод, попробуйте ещё раз" << endl;
     }
-    int rescount = char_count[0] - '0';
+    int res_rocks_number = char_rocks_number[0] - '0';
 
-    rows_num[resnum-1] -= rescount;
+    rocks_num[res_row_number - 1] -= res_rocks_number;
 }
 
 void ai_move()
 {
-    int nim = nim_sum();
+    int nim_s = nim_sum();
     int i = 0;
-    if (nim == 0)
+    if (nim_s == 0)
     {
-        while (rows_num[i] == 0)
+        while (rocks_num[i] == 0)
         {
             i++;
         }
-        int rand_num = generate_rand_num(rows_num[i]);
+        int rand_num = generate_rand_num(rocks_num[i]);
         cout << "Я забираю " << rand_num << " камень(камней) из кучи номер # "<< i+1 << ".\n";
-        rows_num[i] -= rand_num;
+        rocks_num[i] -= rand_num;
     }
     else
     {
-        while(((rows_num[i] ^ nim) >= rows_num[i]) && (i < rows_num.size()))
+        while(((rocks_num[i] ^ nim_s) >= rocks_num[i]) && (i < rocks_num.size()))
         {
             i++;
         }
-        cout << "Я забираю "<< rows_num[i] - (rows_num[i] ^ nim) << " камней из кучи # "<< i+1 << ".\n";
-        rows_num[i] = (rows_num[i] ^ nim);
+        cout << "Я забираю " << rocks_num[i] - (rocks_num[i] ^ nim_s) << " камень(камней) из кучи # " << i + 1 << ".\n";
+        rocks_num[i] = (rocks_num[i] ^ nim_s);
     }
 }
 
@@ -184,7 +182,7 @@ void new_move()
 void win()
 {
     if(current_move % 2 == 0) {
-        cout << "УРА УРА УРА ПОБЕДА!" << endl;
+        cout << "УРА ПОБЕДА!" << endl;
         cout <<
              "░░░░░░░░░░░░░░░░░░░░\n"
              "░░░░░░░░░░░░░░░░░░░░\n"
